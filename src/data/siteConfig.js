6097afa70726091ab0e719720a65bf1f5fb68428
isdeyeter <edestechbilisim@gmail.com>
2026-02-21 16:25:49 +0300

    İlk commit

--- src/data/siteConfig.js
+++ src/data/siteConfig.js
@@ -1,5 +1,5 @@
 export const siteConfig = {
-  phone: "902121234567",     // Başında 0 yok, +90 olarak düşün
-  whatsapp: "905325000999",  // WhatsApp numarası (ülke koduyla)
+  phone: "905412465429",     // Başında 0 yok, +90 olarak düşün
+  whatsapp: "905412465429",  // WhatsApp numarası (ülke koduyla)
   email: "iletisim@istanbulegitimmerkezi.com"
 };
